3d548ab56bd7af506219f3a11aff2aa84cacddb0
usmanmirCS <usman.s.mir@gmail.com>
2021-04-29 01:24:42 -0600

    Intial

---
+++ VR Bowling/Assets/Scripts/Reseter.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reseter : MonoBehaviour
+{
+    public Transform m_ballSpawn;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Bowling Ball")
+        {
+            other.transform.position = m_ballSpawn.position;
+            other.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+}
